b67014cda13bf16137d7d1aeb9089fb9e2a32817
Rowan Katekar <rjkat@ieee.org>
2020-04-01 22:15:53 +1100

    collude work in progress

--- client/js/client.js
+++ client/js/client.js
@@ -5,15 +5,32 @@ export class AnagrindClient {
         this.grid = grid;
         const self = this;
         const socket = io(hostname);
+        
+        socket.on('crosswordShared', gridId => {
+            console.log('got crosswordShared: ' + gridId);
+        });
+        
+        socket.on('fillCell', self.fillCell);
+
+        socket.on('noSuchGrid', gridId => {
+            console.log('no such grid: ' + gridId);
+        });
+
+        socket.on('gridJoined', self.gridJoined);
+
         socket.emit('joinGrid', grid);
-        socket.on("welcome", (message) => {
-            console.log("got welcome: " + message);
-        });
-        socket.on("fillCell", function (event) {
-            console.log("got fillCell" + JSON.stringify(event));
-            self.display.fillCell(event.clueid, event.offset, event.value, true);
-        });
+
         this.socket = socket;
+    }
+
+    gridJoined(msg) {
+        console.log('joined grid: ' + msg.gridId);
+        msg.eventLog.forEach(this.fillCell);
+    }
+
+    fillCell(event) {
+        console.log('got fillCell' + JSON.stringify(event));
+        this.display.fillCell(event.clueid, event.offset, event.value, true);
     }
 
     sendUpdate(event) {
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const fs = require('fs')
 const path = require('path')
 const keyFile = path.join(__dirname, 'server.key')
 const certFile = path.join(__dirname, 'server.cert')
+const shortid = require('shortid')
 
 // const server = https.createServer({
 //     key: fs.readFileSync(keyFile),
@@ -25,20 +26,57 @@ app.get("/grid/:name", function(res, req) {
     next('route');
 });
 
-var io = require('socket.io')(server);
+const io = require('socket.io')(server);
+const grids = {};
 
-io.on("connection", function(socket) {
-    socket.on("joinGrid", function(grid) {
-        socket.join(grid, function() {
-            console.log("join grid: " + grid);
+io.on('connection', function(socket) {
+    socket.on('shareCrossword', function(args) {
+        const gridId = shortid.generate();
+        const solvers = [];
+        solvers.push({socketId: socket.id, id: 0, name: args.name});
+        grids[gridId] = {
+            crossword: args.crossword,
+            solvers: solvers,
+            eventLog: []
+        };
+        socket.join(gridId, function() {
+            socket.emit('crosswordShared', gridId);
         });
-        socket.emit("welcome", "gday");
     });
-    socket.on("fillCell", function(event) {
-        console.log("fillCell event: " + JSON.stringify(event));
-        const grid = Object.keys(socket.rooms);
-        console.log("grid " + grid);
-        socket.to(grid[1]).emit("fillCell", event);
+    socket.on('joinGrid', function(args) {
+        const grid = grids[args.gridId];
+        if (!grid) {
+            socket.emit('noSuchGrid', args.gridId);
+            return;
+        }
+        socket.join(args.gridId, function() {
+            console.log('join ' + args.name + 'to grid: ' + args.gridId);
+            const solverId = grid.solvers.length;
+            // hack: just replay all the packets to everyone who joins
+            socket.emit('gridJoined', {
+                gridId: args.gridId,
+                solverId: solverId,
+                solvers: grid.solvers,
+                crossword: grid.crossword,
+                events: grid.eventLog
+            });
+            grid.solvers.push({
+                id: solverId,
+                socketId: socket.id,
+                name: args.name
+            });
+        });
+    });
+    socket.on('fillCell', function(event) {
+        console.log('fillCell event: ' + JSON.stringify(event));
+        const rooms = Object.keys(socket.rooms);
+        if (rooms.length <= 1) {
+            return;
+        }
+        const gridId = rooms[1];
+        console.log('grid ' + gridId);
+        grids[gridId].eventLog.push(event);
+        socket.to(grid).emit('fillCell', event);
     });
 });
 
