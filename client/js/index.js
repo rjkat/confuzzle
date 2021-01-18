051980931f18032184cb7d701cff985b45ec6d4b
Rowan Katekar <rjkat@ieee.org>
2021-01-18 17:52:47 +1100

    turn on service worker again

--- client/js/index.js
+++ client/js/index.js
@@ -9,8 +9,8 @@ const vm = new App({
     el: '#app',
 });
 
-// if ('serviceWorker' in navigator) {
-//   const sw = 'service-worker.js';
-//   navigator.serviceWorker.register(sw);
-// }
+if ('serviceWorker' in navigator) {
+  const sw = 'service-worker.js';
+  navigator.serviceWorker.register(sw);
+}
 
